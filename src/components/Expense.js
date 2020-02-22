6eaed16f14ff7e940841cf78ff057e6646627a10
Vinicius C. Bonifácio <vinicius.bonifacio1985@gmail.com>
2020-02-22 23:02:07 +0200

    Implemets Balance Component Logic

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useContext } from "react";
+import { GlobalContext } from "../context/GlobalState";
 
 const Expense = () => {
+  const { transations } = useContext(GlobalContext);
   return (
     <div className="inc-exp-container">
       <div>
