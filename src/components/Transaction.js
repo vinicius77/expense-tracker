93110f568c71ad1cb02560483e2a935a2854d048
Vinicius C. Bonifácio <vinicius.bonifacio1985@gmail.com>
2020-02-22 22:52:57 +0200

    Implements the logic to render style based on negative or positive amounts

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,10 +1,14 @@
 import React from "react";
 
 const Transaction = ({ transaction }) => {
+  const sign = transaction.amount < 0 ? "-" : "+";
   return (
     <div>
-      <li className="minus">
-        {transaction.text} <span>{transaction.amount}</span>
+      <li className={transaction.amount < 0 ? "minus" : "plus"}>
+        {transaction.text}{" "}
+        <span>
+          {sign}${Math.abs(transaction.amount)} {/*Abslute Number*/}
+        </span>
         <button className="delete-btn">X</button>
       </li>
     </div>
