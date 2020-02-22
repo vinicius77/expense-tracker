70aff901056b07098f06486e6067510f1a720241
Vinicius C. Bonifácio <vinicius.bonifacio1985@gmail.com>
2020-02-22 23:12:29 +0200

    Implements logic of Expense Component

--- src/components/Expense.js
+++ src/components/Expense.js
@@ -2,16 +2,30 @@ import React, { useContext } from "react";
 import { GlobalContext } from "../context/GlobalState";
 
 const Expense = () => {
-  const { transations } = useContext(GlobalContext);
+  const { transactions } = useContext(GlobalContext);
+
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const income = amounts
+    .filter(amount => amount > 0)
+    .reduce((total, amount) => (total += amount), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts
+      .filter(amount => amount < 0)
+      .reduce((total, amount) => (total += amount)) * -1
+  ).toFixed(2);
+
   return (
     <div className="inc-exp-container">
       <div>
         <h4>Income</h4>
-        <p className="money plus">+$0.00</p>
+        <p className="money plus">+${income}</p>
       </div>
       <div>
         <h4>Expense</h4>
-        <p className="money minus">-$0.00</p>
+        <p className="money minus">-${expense}</p>
       </div>
     </div>
   );
